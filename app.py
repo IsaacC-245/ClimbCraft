374f7a7be0c58e09f77be117d330939625f2fe79
RyanEbsen <77689652+RyanEbsen@users.noreply.github.com>
2024-04-12 23:44:25 -0500

    cool stuff

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from forms import RegistrationForm, LoginForm, SearchForm, NewEmployerForm, EditEmployerForm, \
+    RelationForm, DeleteEmployerForm, AddAdminForm
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    email_confirmed = db.Column(db.Boolean, nullable=False, default=False)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if request.method == "POST" and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        if not user:
+            flash(f"No account exists with that email", "danger")
+            return redirect(url_for("login"))
+        if not user.email_confirmed:
+            flash(f"Please activate your account before loging in", "danger")
+            return redirect(url_for("login"))
+        if bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for("home"))
+        else:
+            flash(f"invalid credentials", "danger")
+            return redirect(url_for("login"))
+    return render_template("login.html", title="Log in", form=form)
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,55 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, DateField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, Optional
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField("Log in")
+
+
+class SearchForm(FlaskForm):
+    search = StringField("Search", validators=[DataRequired()])
+    submit = SubmitField("Search")
+
+
+class NewEmployerForm(FlaskForm):
+    employer_name = StringField("Employer Name", validators=[DataRequired()])
+    headquarters_address = StringField('Headquarters Address', validators=[DataRequired()])
+    description = StringField('Description', validators=[Optional()])
+    start_date = DateField('Start Date', validators=[DataRequired()], format='%Y-%m-%d')
+    end_date = DateField('End Date', format='%Y-%m-%d', validators=[Optional()])
+    submit = SubmitField('Add Employer')
+
+
+class EditEmployerForm(FlaskForm):
+    employer_name = StringField("Employer Name", validators=[DataRequired()])
+    headquarters_address = StringField('Headquarters Address', validators=[Optional()])
+    description = StringField('Description', validators=[Optional()])
+    start_date = DateField('Start Date', validators=[Optional()], format='%Y-%m-%d')
+    end_date = DateField('End Date', format='%Y-%m-%d', validators=[Optional()])
+    submit = SubmitField('Edit Employer')
+
+
+class DeleteEmployerForm(FlaskForm):
+    employer_name = StringField("Employer Name", validators=[DataRequired()])
+    submit = SubmitField('Delete Employer')
+
+
+class RelationForm(FlaskForm):
+    parent_name = StringField("Parent Name", validators=[DataRequired()])
+    child_name = StringField('Child Name', validators=[DataRequired()])
+    submit = SubmitField('Add Relation')
+
+
+class AddAdminForm(FlaskForm):
+    email_address = StringField("Email Address", validators=[DataRequired()])
+    submit = SubmitField("Create Admin")
